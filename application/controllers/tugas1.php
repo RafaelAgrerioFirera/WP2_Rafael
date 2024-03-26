aa3f53189e011042b2ecd1c18f6e1d4c1892bc38
RafaelAgrerioFirera <rafaelagreriofirerarafael@gmail.com>
2024-03-26 10:32:19 +0700

    tugas1

---
+++ application/controllers/tugas1.php
@@ -0,0 +1,12 @@
+<?php
+class tugas1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Rafael Agrerio Firera
+ Saya tinggal di Sungai Ambawang
+ olah raga yang saya sukai adalah bermain
+ Badminton";
+ }
+}
