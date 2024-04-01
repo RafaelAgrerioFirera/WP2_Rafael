1238e4b4993feeb34542d28a69c1ed7181d047ec
RafaelAgrerioFirera <rafaelagreriofirerarafael@gmail.com>
2024-04-01 15:30:45 +0700

    pertemuan 3

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,18 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"Selamat Datang.. selamat belajar Web Programming";
+        //this->load->view('view-Latihan1);
+    }
+
+
+    public function penjumlahan($n1, $n2)
+    {
+    $this->load->model('Model_Latihan1');
+    $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
+    echo "Hasil Penjumlahan dari". $n1 ." + " . $n2 . " = " .$hasil;
+
+    }
+}
