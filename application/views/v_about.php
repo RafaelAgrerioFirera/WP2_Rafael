2e5f9ecdb23b843ab3816cba704cddc7f35416b1
RafaelAgrerioFirera <rafaelagreriofirerarafael@gmail.com>
2024-04-22 16:14:44 +0700

    pertemuan 4 dan tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+  <h1><?php echo $judul ?></h1>
+  <h4>Nama</h4>
+  <ul type="disc">
+     <li>Nama Pembuat : Rafael Agrerio Firera</li>
+     <li>Nama Panggilan : Rafael</li>
+  </ul>
+  <br>
+  <h4>Alamat</h4>
+  <ul type="none">
+    <li> Sungai Ambawang Dusun Pancaroba</li>
+  </ul>
+ 
+  <h4>Tempat Lahir</h4>
+  <ul type="none">
+  <li>Tapah</li>
+  </ul>
+  <h4>Olah Raga Favorit</h4>
+  <ul type="square">
+    <li>Bulutangkis</li>
+    <li>Catur</li>
+    <li>Futsal</li>
+  </ul>
+</section>
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+       <footer>
+           <a href="http://www.RentalBuku.com">RafaelBuku</a>
+       </footer>
+    </div>
+</body>
+</html>
