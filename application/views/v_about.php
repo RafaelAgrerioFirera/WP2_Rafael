407ced64b0a2e26dd1e134eab73f7d12a9eecfe9
RafaelAgrerioFirera <rafaelagreriofirerarafael@gmail.com>
2024-04-29 17:12:21 +0700

    Pertemuan6 dan Tugas5

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,13 +2,13 @@
   <h1><?php echo $judul ?></h1>
   <h4>Nama</h4>
   <ul type="disc">
-     <li>Nama Pembuat : Rafael Agrerio Firera</li>
+     <li>Nama Pembuat : Rafael Agrerio Firera </li>
      <li>Nama Panggilan : Rafael</li>
   </ul>
   <br>
   <h4>Alamat</h4>
   <ul type="none">
-    <li> Sungai Ambawang Dusun Pancaroba</li>
+    <li>Sungai Ambawang Dusun Pancaroba</li>
   </ul>
  
   <h4>Tempat Lahir</h4>
@@ -20,5 +20,7 @@
     <li>Bulutangkis</li>
     <li>Catur</li>
     <li>Futsal</li>
+    <li>Bola Kaki</li>
+    <li>Mobile Legends</li>
   </ul>
 </section>
